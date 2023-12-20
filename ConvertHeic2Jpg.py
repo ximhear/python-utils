8c69cf84bad46e7f495b34ad19c53ef916a6b38e
chlee85095 <chlee85095@cnspartner.com>
2023-12-20 13:46:58 +0900

    .

--- ConvertHeic2Jpg.py
+++ ConvertHeic2Jpg.py
@@ -4,6 +4,7 @@ import io
 import os
 import subprocess
 
+
 def convert_heic_to_jpeg(heic_path, jpeg_path, width=1024):
     # Read the HEIC file
     heif_file = pyheif.read(heic_path)
@@ -18,6 +19,7 @@ def convert_heic_to_jpeg(heic_path, jpeg_path, width=1024):
     # Resize and save as JPEG
     img.resize((width, height), Image.ANTIALIAS).save(jpeg_path, format="JPEG")
 
+
 def convert_all_heic_to_jpg(folder_path):
     for file in os.listdir(folder_path):
         if file.lower().endswith('.heic'):
@@ -26,6 +28,7 @@ def convert_all_heic_to_jpg(folder_path):
             convert_heic_to_jpeg(heic_path, jpeg_path)
             print(f"Converted {file} to JPEG.")
 
+
 def convert_heic_to_jpg_with_imagemagick(input_folder, width=1024):
     files = [f for f in os.listdir(input_folder) if f.lower().endswith('.heic')]
     for file in files:
@@ -33,5 +36,7 @@ def convert_heic_to_jpg_with_imagemagick(input_folder, width=1024):
         output_path = os.path.join(input_folder, os.path.splitext(file)[0] + '.jpg')
         subprocess.run(['magick', 'convert', input_path, '-resize', str(width), output_path])
         print(f"Converted {file} to JPG.")
+
+
 # Example usage
 convert_heic_to_jpg_with_imagemagick('/Users/gzonelee/PycharmProjects/pythonProject1')
