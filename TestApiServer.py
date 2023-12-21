3a6ba99fa358ddcd5cc963663209fc7cb1d04a3a
chlee85095 <chlee85095@cnspartner.com>
2023-12-21 15:56:08 +0900

    upload api

--- TestApiServer.py
+++ TestApiServer.py
@@ -25,16 +25,29 @@ def api_endpoint():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    # 텍스트 필드 데이터 접근
+    file_name = request.form.get('file-url')  # 파일 이름 필드
+    creation_date = request.form.get('date')  # 생성일 필드
+
+    # 로그 기록
+    app.logger.info(f'Received file_name: {file_name}, creation_date: {creation_date}')
+
+    # 파일 데이터 접근
     if 'file' not in request.files:
         return jsonify({"message": "No file part"})
     file = request.files['file']
     if file.filename == '':
         return jsonify({"message": "No selected file"})
+
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return jsonify({"message": "File uploaded successfully", "filename": filename})
-
+        return jsonify({
+            "message": "File uploaded successfully",
+            "file_name": file_name,
+            "creation_date": creation_date,
+            "saved_file": filename
+        })
 
 if __name__ == "__main__":
     app.run(debug=True)
